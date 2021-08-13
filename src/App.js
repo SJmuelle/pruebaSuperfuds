edab39e9231f90eb5ef799c81adee7ed92f3c921
SHIRLEY  MUELLE CAMARGO <62040229+SJmuelle@users.noreply.github.com>
2021-08-13 17:04:02 -0500

    Actualizaciones de bugs Carrito de compras
    Actualizaciones de bugs del carrito de compra, activacion de los link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import Products from './components/Products';
 import Navbar from './components/Navbar';
 import './App.css';
-// import CheckoutPage from './components/CheckoutPages';
+import CheckoutPage from './components/CheckoutPages';
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 
@@ -13,7 +13,9 @@ function App() {
       <div className="App">
         <Navbar />
         <Switch>
- 
+          <Route path="/checkout-pages">
+            <CheckoutPage />
+          </Route>
           <Route path="/">
             <Products />
           </Route>
